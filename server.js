894470867818e20e8e610fd8ff80b29b5457012e
Wojciech Olejnik <wojciecholejnik5@gmail.com>
2021-01-16 11:36:51 +0100

    add chat functions and init server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const messages = [];
+
+app.use(express.static(path.join(__dirname + '/client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/index.html'));
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
